b888baa2ce7f8052cc185de504e7f14f0cddb676
rishabmadaan <44662075+rishabmadaan@users.noreply.github.com>
2020-03-10 00:41:39 -0700

    Update SupplyChain.sol

--- SupplyChain.sol
+++ SupplyChain.sol
@@ -7,7 +7,7 @@ contract SupplyChain
     string public company_name;
     
     address payable companyAddress;
-    
+    uint distributor_profit2=1 ether;
         struct Order{
        
         string name;
@@ -107,6 +107,16 @@ contract SupplyChain
           string status,
           uint orderId
             );
+            
+            
+      event orderCreatedforCompany(
+       
+            string name,
+        uint quantity,
+        string status,
+        uint orderId,
+        uint orderPayment
+        );
    
    
 function giveOrder(string memory _Componentname, uint number, address _partnerCompany)public payable
@@ -202,8 +212,86 @@ function giveOrder(string memory _Componentname, uint number, address _partnerCo
        
        
     }
-   
-   
+    
+    
+      
+  // This function is used to set menu for product components' name in a product
   
+   function setProductComponents() public returns(string memory){
+        productToComponentMapping["DesignedShirt"].push("rawShirt");
+        productToComponentMapping["DesignedShirt"].push("thread");
+        return "components have been set";
+       }
+    
+    // The distributor sends his requirement to the company, mentioning the productname and quantity required
+     function distributorRequirement(string memory _productName, uint quantity) public payable{
+        product[] memory productList= products[companyAddress];
+   
+         Order memory order1;
+        order1.status="pending";
+        order1.quantity=quantity;
+        order1.name=_productName;
+       
+        uint totalPrice = 0;
+       
+        for (uint i = 0; i < productList.length; i++) {
+        
+            product memory product1=products[companyAddress][i];
+            string memory productOrderedName = product1.name;
+            if( (keccak256(abi.encodePacked((productOrderedName))) == keccak256(abi.encodePacked((_productName))) ))
+            {
+                totalPrice=products[companyAddress][i].price*quantity;
+            }
+   
+        }
+       
+        require(msg.value==totalPrice);
+        order1.orderPayment=msg.value;
+        partnerOrders[companyAddress].push(order1);
+     emit orderCreatedforCompany(order1.name, order1.quantity,order1.status,order1.orderId,order1.orderPayment);
+       
+    }
+    
+    // The Company checks stock available for the distributor's order and sends the products to the distributor and pays the transportation fees 
+   
+     function checkStock(uint _orderId, address _distributor,address payable _transporter ) public payable{ // change according to new manufacturer 2 layer
+    
+        Order memory order1=partnerOrders[msg.sender][_orderId];
+        order1.status="completed";
+        string memory _productOrderedName = order1.name; // to set the inventory of component we have fetched component name from
+        inventory memory inventory1= companyInventory[companyAddress][order1.name];
+        inventory1.productCount=inventory1.productCount- order1.quantity;
+        companyInventory[companyAddress][_productOrderedName] = inventory1;
+        partnerOrders[msg.sender][_orderId]=order1;
+        msg.sender.transfer(order1.orderPayment);
+       
+        inventory memory inventory2= companyInventory[_distributor][order1.name];
+        inventory2.productCount=inventory2.productCount+1;
+        
+        companyInventory[_distributor][order1.name]=inventory2;
+     
+        uint transportation_cost=order1.orderPayment/100; // he should know what transportation he has to pay
+       
+        
+        product memory product2= products[companyAddress][_orderId];
+        product2.owner=_distributor;
+        product2.price=product2.price+distributor_profit2;
+        
+        products[_distributor].push(product2);
+        
+        transportStocktoDistributor(transportation_cost,_transporter,_distributor, order1.name ) ;
+      }
+   
+   //This function is used by the company which is automatically called to pay the transporter account
+   
+    function transportStocktoDistributor(uint _transportationcost, address payable transporteraddress, address _distributor, string memory _name) public payable
+    {
+       
+        transporteraddress.transfer(_transportationcost);
+    }
+   
+    
+    
+   
     
 }
